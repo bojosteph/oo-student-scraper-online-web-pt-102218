5e86f04156707398c722fc4e8db96560ead6368d
Roberto Samia <bojosteph@gmail.com>
2018-12-10 01:53:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,13 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url="./fixtures/student-site/index.html")
+    student_hash = {}
+    html = Nokogiri::HTML(open(index_url))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +15,8 @@ class Scraper
   end
 
 end
+
+
+# :name html.css("h4.student-name").text
+# :location html.css("p.student-location").text
 
